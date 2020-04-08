1e55a9839b4f85f06a31d5cdea03259d5a59dec9
cybarwarfare <hafismuhammed984@gmail.com>
2020-04-08 21:27:22 +0530

    login page

--- src/App.js
+++ src/App.js
@@ -3,41 +3,18 @@ import './App.css';
 // import ProductCard from './component/productcard/productCard';
 // import Addproduct from './component/AddProduct/addProduct';
 // import SideMenu from './component/leadAdder/sideMenu'
-import CallcheduleCard from './component/followUp/callScheduleCard/callScheduleCard'
-import EmailSchedule from './component/followUp/emailScheduleCard/emailScheduleCard'
-import CallTask from './component/followUp/callTask/callTask'
-import EmailTask from './component/followUp/emailTask/emailTask'
-import CallHistory from './component/followUp/callHistory/callHistory'
-import EmailHistory from './component/followUp/emailHistory/emailHistory'
+// import CallcheduleCard from './component/followUp/callScheduleCard/callScheduleCard'
+// import EmailSchedule from './component/followUp/emailScheduleCard/emailScheduleCard'
+// import CallTask from './component/followUp/callTask/callTask'
+// import EmailTask from './component/followUp/emailTask/emailTask'
+// import CallHistory from './component/followUp/callHistory/callHistory'
+// import EmailHistory from './component/followUp/emailHistory/emailHistory'
+import Login from './component/logIn/logIn'
 
 function App(){
   return (
-    <div class="App d-flex flex-row ">
-      <div class="d-flex flex-column my-view">
-          <CallcheduleCard></CallcheduleCard>
-          <EmailSchedule></EmailSchedule>
-          <EmailSchedule></EmailSchedule>
-          <EmailSchedule></EmailSchedule>
-          <CallcheduleCard></CallcheduleCard>
-          <CallcheduleCard></CallcheduleCard>
-      </div>
-      <div class="d-flex flex-column my-view1 justify-content-lg-around">
-          <CallTask></CallTask>
-          <EmailTask></EmailTask>
-      </div>
-      <div class="d-flex flex-column my-view">
-          <CallHistory></CallHistory>
-          <EmailHistory></EmailHistory>
-          <CallHistory></CallHistory>
-          <EmailHistory></EmailHistory>
-          <CallHistory></CallHistory>
-          <EmailHistory></EmailHistory>
-          <CallHistory></CallHistory>
-          <EmailHistory></EmailHistory>
-          <CallHistory></CallHistory>
-          <EmailHistory></EmailHistory>
-          
-      </div>
+    <div class="App d-flex flex-row">
+      <Login></Login>
     </div>
   );
 }
---
+++ src/component/logIn/logIn.js
@@ -0,0 +1,63 @@
+import React from 'react'
+import './logIn.css'
+
+class Login extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            value : 'admin',
+            inputcolor:"#92d9a4",
+            bgcolor:"#62c888"
+        };
+    }
+    adminlog=()=>{
+        this.setState({
+            value:'admin',
+            inputcolor:"#92d9a4",
+            bgcolor:"#62c888"
+        })
+    }
+    salelog=()=>{
+        this.setState({
+            value:'sales',
+            inputcolor:"#3b7fde",
+            bgcolor:"#1a509b"
+        })
+    }
+    financelog=()=>{
+        this.setState({
+            value:'finance',
+            inputcolor:"#9261d1",
+            bgcolor:"#4d2580"
+        })  
+    }
+
+
+    render(){
+        return(
+        <div class="log-main bg-white d-flex justify-content-center align-items-center">
+            <div class="log-condainer d-flex flex-row bg-white">
+                {/* first half */}
+                <div class="d-flex flex-column justify-content-center align-items-center" style={{width:"50vw"}}>
+                    <div>
+                        <img src="companylogo.png"class="logo"/>
+                    </div>
+                    <div class="d-flex flex-row mt-5">
+                        <div class="log-buttons mr-sm-4"type='button'onClick={this.adminlog}><snap>Admin</snap></div>
+                        <div class="log-buttons mr-sm-4"type='button'onClick={this.salelog}><snap>Sales</snap></div>
+                        <div class="log-buttons mr-sm-4"type='button'onClick={this.financelog}><snap>Finance</snap></div>
+                    </div>
+                </div>
+                {/* second half */}
+                <div class="log-secondhalf d-flex flex-column justify-content-center align-items-center"style={{backgroundColor: this.state.bgcolor}}>
+                    <h2 class="mb-sm-4 text-white" style={{fontSize:"5vh"}}>Welcome back</h2> 
+                    <input type="text"class="log-input w-50 mb-4 p-3" style={{backgroundColor: this.state.inputcolor}}name="name" placeholder="Email address"/>
+                    <input type="password"class="log-input w-50 mb-4 p-3"style={{backgroundColor: this.state.inputcolor}}name="name" placeholder="Password"/>
+                    <div class="log-button d-flex justify-content-center align-items-center text-white font-weight-bold"type='button'><snap>logIn</snap></div>
+                </div>
+            </div>
+        </div>
+        )
+    }
+}
+export default Login
